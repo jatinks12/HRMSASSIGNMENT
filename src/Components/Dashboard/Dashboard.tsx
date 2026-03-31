c83b4bd1c85fba7e0049b933a7eea305e1431a00
Vivek <00079@email.com>
2026-03-31 16:50:50 +0530

    fetch data

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -1,29 +1,85 @@
+import { useEffect, useState } from "react";
 import "./Dashboard.css"
+import { SupabaseClient } from "../../Helper/Supabase";
+
+
 const Dashboard = () => {
+
+  const [employees, setEmployees] =useState<any[]>([])
+
+  useEffect(() => {
+    getEmployees();
+  }, []);
+
+  async function getEmployees() {
+    const { data, error } = await SupabaseClient
+      .from("Employee") 
+      .select("*");
+
+    if (error) {
+      console.log(error);
+    } else {
+      setEmployees(data);
+      console.log(data);
+    }
+  }
+
   return (
     <div className="dashboard">
 
-      
-
-      {/* Cards Section */}
+     
       <div className="cards-container">
         
         <div className="card">
-          <h3 >Total Employees</h3>
-         
+          <h3>Total Employees</h3>
+          <p>{employees.length}</p>
         </div>
 
         <div className="card">
           <h3>Leaves</h3>
-         
         </div>
 
         <div className="card">
           <h3>Pending Requests</h3>
-          
         </div>
 
       </div>
+
+      {/* Table */}
+      <table >
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Department</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+
+        <tbody>
+      
+  {employees && employees.length > 0 ? (
+    employees.map((emp) => (
+      <tr key={emp.id}>
+        <td>{emp.Name}</td>
+        <td>{emp.Email}</td>
+        <td>{emp.department}</td>
+        <td>{emp.role}</td>
+      </tr>
+    ))
+  )
+  
+  
+  
+  : (
+    <tr>
+      <td  style={{ textAlign: "center" }}>
+        No Data Found
+      </td>
+    </tr>
+  )}
+</tbody>
+      </table>
 
     </div>
   );
