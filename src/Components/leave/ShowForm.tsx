1bf23ef7b17a0b8a75debd94e951e102cb7eb968
jatinks12 <Jatinkumarsharma307@gmail.com>
2026-04-01 15:01:21 +0530

    add showForm UI

--- src/Components/leave/ShowForm.tsx
+++ src/Components/leave/ShowForm.tsx
@@ -1,31 +1,86 @@
-import {  useFormik } from "formik"
+import { useFormik } from "formik";
 
-import { observer } from "mobx-react-lite"
-import FormStore from "./formstore"
+import { observer } from "mobx-react-lite";
+import FormStore from "./formstore";
+import styles from "./ShowForm.module.css";
+import *as Yup from "yup"
+import { SupabaseClient } from "../../Helper/Supabase";
 
 const ShowForm = () => {
-  const formik=useFormik({
-    initialValues:{
-      name:FormStore.name,
-    
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      startDate: "",
+      endDate: "",
+      reason: "",
     },
-  onSubmit:()=>{ console.log("how are you")}
-
-  })
+    validationSchema:Yup.object({
+      name:Yup.string().required("Name is required"),
+      startDate:Yup.string().required("Start Date is required"),
+      endDate:Yup.string().required("End Date is required"),
+      reason:Yup.string().required("Reason is requried"),
+    }),
+    onSubmit: async(values) => {
+      console.log(values);
+     
+    },
+  });
 
   return (
-     <form onSubmit={formik.handleSubmit}>
-      <div>
-        <span>Personal information   </span>
-        <button type="button" onClick={()=>FormStore.handleFormCancel()}>cancel</button>
+    <form onSubmit={formik.handleSubmit} className={styles.form}>
+      <div className={styles.header}>
+        <span>Personal information </span>
+        <button type="button" onClick={() => FormStore.handleFormCancel()} className={styles.cancelBtn}>
+          cancel
+        </button>
       </div>
-       <div>
+      <div className={styles.formGroup}>
         <label htmlFor="name">Name</label>
-        <input id="name" name="name" type="text" value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur}></input>
-       </div>
-      {formik.touched.name && formik.errors.name &&<div>{formik.errors.name}</div>}
-      </form>
-  )
-}
+        <input
+          id="name"
+          name="name"
+          type="text"
+          placeholder="Enter the name"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        ></input>
+        {formik.touched.name && formik.errors.name && <div className={styles.error}>{formik.errors.name}</div>}
+        <label htmlFor="startDate">startDate</label>
+        <input
+          id="startDate"
+          name="startDate"
+          type="datetime-local"
+          value={formik.values.startDate}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        ></input>
+         {formik.touched.startDate && formik.errors.startDate && <div  className={styles.error}>{formik.errors.startDate}</div>}
+        <label htmlFor="endDate">endDate</label>
+        <input
+          id="endDate"
+          name="endDate"
+          type="datetime-local"
+          value={formik.values.endDate}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        ></input>
+         {formik.touched.endDate && formik.errors.endDate && <div  className={styles.error}>{formik.errors.endDate}</div>}
+        <label htmlFor="reason">reason</label>
+        <input
+          id="reason"
+          name="reason"
+          type="text"
+          placeholder="Enter your reason"
+          value={formik.values.reason}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        ></input>
+          {formik.touched.reason && formik.errors.reason && <div  className={styles.error}>{formik.errors.reason}</div>}
+        <button type="submit" className={styles.submitBtn}>Submit</button>
+      </div>
+    </form>
+  );
+};
 
-export default observer(ShowForm)
+export default observer(ShowForm);
